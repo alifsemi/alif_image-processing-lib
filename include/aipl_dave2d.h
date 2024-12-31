f71ba78dfe992b92431115da3596ab1447ff96c4
Yevhen Bobovskyi <yevhen.bobovskyi@alifsemi.com>
2024-12-31 12:14:25 +0200

    Add license info

--- include/aipl_dave2d.h
+++ include/aipl_dave2d.h
@@ -1,6 +1,18 @@
-/**
- * @file aipl_dave2d.h
+/* Copyright (C) 2022-2024 Alif Semiconductor - All Rights Reserved.
+ * Use, distribution and modification of this code is permitted under the
+ * terms stated in the Alif Semiconductor Software License Agreement
+ *
+ * You should have received a copy of the Alif Semiconductor Software
+ * License Agreement with this file. If not, please write to:
+ * contact@alifsemi.com, or visit: https://alifsemi.com/license
+ *
  */
+
+/******************************************************************************
+ * @file    aipl_dave2d.h
+ * @brief   D/AVE2D related functions definition
+ *
+******************************************************************************/
 
 #ifndef AIPL_DAVE2D_H
 #define AIPL_DAVE2D_H
